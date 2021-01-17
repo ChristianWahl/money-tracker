4206f5e248323266741b4825983a43a22952a465
ChristianWahl <contact@christian-wahl.com>
2021-01-17 09:58:34 +0100

    fixed Dockerfile

--- src/containers/WorkInProgress.js
+++ src/containers/WorkInProgress.js
@@ -6,6 +6,6 @@ export default () => (
     size="large"
     icon="hourglass half"
     header="Work in Progress"
-    content="Hello Udacity 😉"
+    content="This page is not ready yet, check back later 😉"
   />
 );
